ec79c1e9cfa6569c7415806eddf18ed5644225af
rmdocherty <rmdocherty2@gmail.com>
2026-03-29 18:42:54 +0100

    fix config

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  worker: {
+    format: 'es',
+  },
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        // REMOVE format: 'iife'
+      }
+    }
+
   },
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin'
     }
+  },
+  preview: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
   assetsInclude: ['**/*.wasm'],
   publicDir: 'public'
